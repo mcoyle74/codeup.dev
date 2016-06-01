e93abc0621560ebad56a38cd37c4997aec50dd87
Michael Coyle <MichaelCoyle@Coyle-Mac.local>
2016-06-01 15:13:37 -0500

    Replaced if statement values (number of grades, average, discount) with variables

--- public/js/if-else.js
+++ public/js/if-else.js
@@ -4,7 +4,11 @@ var grade1 = 70;
 var grade2 = 80;
 var grade3 = 95;
 
-if ((grade1 + grade2 + grade3) / 3 > 80) {
+var sumOfGrades = grade1 + grade2 + grade3;
+var numberOfGrades = 3;
+var awesomeAverage = 80;
+
+if ((grade1 + grade2 + grade3) / numberOfGrades > awesomeAverage) {
 	console.log("You\'re awesome")
 } else {
 	console.log("You need to practice more")
@@ -14,21 +18,22 @@ var cameron = 180;
 var ryan = 250;
 var george = 320;
 var discountThreshold = 200;
+var discount = .35
 
 if (cameron > discountThreshold) {
-	console.log("Cameron pays $" + cameron + "before the discount and $" + (cameron * .65) + "after the discount");
+	console.log("Cameron pays $" + cameron + "before the discount and $" + (cameron - (cameron * discount)) + "after the discount");
 } else {
 	console.log("Cameron pays $" + cameron + ", no discount");
 }
 
 if (ryan > discountThreshold) {
-	console.log("Ryan pays $" + ryan + " before the discount and $" + (ryan * .65) + " after the discount");
+	console.log("Ryan pays $" + ryan + " before the discount and $" + (ryan - (ryan * discount)) + " after the discount");
 } else {
 	console.log("Rameron pays $" + ryan + ", no discount");
 }
 
 if (george > discountThreshold) {
-	console.log("George pays $" + george + " before the discount and $" + (george * .65) + " after the discount");
+	console.log("George pays $" + george + " before the discount and $" + (george - (george * discount)) + " after the discount");
 } else {
 	console.log("George pays $" + george + ", no discount");
 }

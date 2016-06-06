9430c86abffc63592d8ef91e0359e439f4c44b82
Michael Coyle <mcoyle74@gmail.com>
2016-06-06 13:28:13 -0500

    Simplified planets-string.js and fixed output to include </li> tags

--- public/js/planets-string.js
+++ public/js/planets-string.js
@@ -17,15 +17,9 @@
     // Bonus: Create a second string that would display your planets in an undordered list.
     //        You will need an opening AND closing <ul> tags around the entire string, and <li> tags around each planet.
     //        console.log() your results.
-
-    for (var i = 0; i <= 7; i++) {
-        planetsArray.splice(i * 2, 0, '<li>');
-    }
     
-    planetsArray.unshift('<ul>');
-    planetsArray.push('</ul>');
+    var planetsStringUL = '<ul><li>' + planetsArray.join('</li><li>') + '</li></ul>';
     
-    var planetsStringUL = planetsArray.join('');
     console.log(planetsStringUL);
 
 })();

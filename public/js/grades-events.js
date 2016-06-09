905e588a044126ffdbcadf9107a9d24df152c30b
Michael Coyle <mcoyle74@gmail.com>
2016-06-09 14:58:55 -0500

    Added function to reenable \'Add and\' buttons and fixed src that connects grades.html to JS file

--- public/js/grades-events.js
+++ public/js/grades-events.js
@@ -1,24 +1,40 @@
 'use strict';
 
 var student = {
-    awesomeGrade: 80,
-    name: null,
-    subjects: [],
-    calculateAverage: function () {
-        var average = 0;
-        this.subjects.forEach(function (subject) {
-            average += subject.grade;
-        });
-        return average / this.subjects.length;
-    },
-    addSubject: function (name, grade) {
-        var subject = {
-            name: name,
-            grade: grade
-        };
-        this.subjects.push(subject);
-    },
-    isAwesome: function () {
-        return this.calculateAverage() > this.awesomeGrade;
-    }
+	awesomeGrade: 80,
+	name: null,
+	subjects: [],
+	calculateAverage: function () {
+		var average = 0;
+		this.subjects.forEach(function (subject) {
+			average += subject.grade;
+		});
+		return average / this.subjects.length;
+	},
+	addSubject: function (name, grade) {
+		var subject = {
+			name: name,
+			grade: grade
+		};
+		this.subjects.push(subject);
+	},
+	isAwesome: function () {
+		return this.calculateAverage() > this.awesomeGrade;
+	}
 }
+
+var saveName = document.getElementById('save-name');
+var enableAddButtons = document.getElementsByClassName('btn');
+
+var enableButtons = function() {
+	var checkNameEntry = document.getElementById('name').value;
+	if (checkNameEntry == null || checkNameEntry == '') {
+		alert('You must enter a name to continue');
+		return false;
+	} else {
+		for (var i = 0; i < enableAddButtons.length; i++) {
+			enableAddButtons[i].disabled = false;
+		}
+	}
+}
+saveName.addEventListener('click', enableButtons, false);

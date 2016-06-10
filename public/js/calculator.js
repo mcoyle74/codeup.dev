5eb8e35dc2acfdf027375faef348e0e6a7048a4f
Michael Coyle <mcoyle74@gmail.com>
2016-06-10 14:26:46 -0500

    Created variables for each button and started functions for calculations and display and an event listener

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,2 +1,44 @@
 "use strict";
 
+var buttonPush = getElementsByClassName('button');
+
+var clearButton = getElementById('clear')
+var plusminusButton = getElementById('equals');
+var percentButton = getElementById('equals');
+
+var oneButton = getElementById('one');
+var twoButton = getElementById('two');
+var threeButton = getElementById('three');
+var fourButton = getElementById('four');
+var fiveButton = getElementById('five');
+var sixButton = getElementById('six');
+var sevenButton = getElementById('seven');
+var eightButton = getElementById('eight');
+var nineButton = getElementById('nine');
+var zeroButton = getElementById('zero');
+
+var pointButton = getElementById('point');
+var divideButton = getElementById('divide');
+var multiplyButton = getElementById('');
+var subtractButton = getElementById('');
+var addButton = getElementById('');
+var equalsButton = getElementById('equals');
+
+var add = function() {
+
+}
+var subtract = function() {
+
+}
+var multiply = function() {
+
+}
+var divide = function() {
+
+}
+
+var inputCalculateDisplay = function () {
+	
+}
+
+buttonPush.addEventListener('click', inputCalculateDisplay, false);

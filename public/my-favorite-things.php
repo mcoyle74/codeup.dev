329fb3c63eb8c249793a74b62fcbc96fae4991c2
Michael Coyle <mcoyle74@gmail.com>
2016-07-13 16:15:24 -0500

    Styled with Bootstrap and some custom CSS.

--- public/my-favorite-things.php
+++ public/my-favorite-things.php
@@ -7,21 +7,36 @@ $favoriteThings	= ['family', 'learning', 'art', 'movies', 'games', 'food'];
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
 	<title>My Favorite Things</title>
+
+	<style type="text/css">
+		body {
+			background-color: #f0f0f0;
+		}
+		.table-striped>tbody>tr:nth-of-type(odd) {
+			background-color: #cfecd2;
+		}
+	</style>
 </head>
 <body>
-	<h1>Michael's Favorite Things</h1>
-	<table>
-		<tr>
-			<th>Favorite Things:</th>
-		</tr>
-		<?php foreach ($favoriteThings as $thing) { ?>
-		<tr>
-			<td>
-				<?php echo $thing; ?>
-			</td>
-		</tr>
-		<?php } ?>
-	</table>
+	<div class="container">
+		<h1>Michael's Favorite Things</h1>
+		<table class="table table-striped">
+			<tr>
+				<th>Favorite Things:</th>
+			</tr>
+			<?php foreach ($favoriteThings as $thing) { ?>
+			<tr>
+				<td>
+					<?php echo $thing; ?>
+				</td>
+			</tr>
+			<?php } ?>
+		</table>
+	</div>
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </body>
 </html>

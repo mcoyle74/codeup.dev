8d32ad8cc740b4a3f4e8a0a212ceecd4c74af41c
Michael Coyle <mcoyle74@gmail.com>
2016-07-15 12:42:30 -0500

    Added if statement to php that checks if $_POST is empty and then checks username and password and redirects to authorized.php is matched or else outputs a login failure message.

--- public/login.php
+++ public/login.php
@@ -1,5 +1,12 @@
 <?php
 
+if ($_POST) {
+	$username = isset($_POST['username']) ? $_POST['username'] : '';
+	$password = isset($_POST['password']) ? $_POST['password'] : '';
+	if (($username == 'guest') && ($password == 'password')) {
+		header('Location: authorized.php');
+	}
+}
 
 
 ?>
@@ -12,11 +19,12 @@
 
 <body>
 	<form method="POST" action="/login.php">
-		<label for="username">Username: </label>
+		<label for="usernumber">Username: </label>
 		<input id="username" name="username" type="text">
 		<label for="password">Password: </label>
 		<input id="password" name="password" type="password">
 		<button type="submit">Submit</button>
 	</form>
+	<p><?= 'Sorry, username or password not valid.' ?></p>
 </body>
 </html>

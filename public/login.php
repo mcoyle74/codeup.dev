dcd1e3eb29e145b9c2ef3bd0a0afb91d2f098b8b
Michael Coyle <mcoyle74@gmail.com>
2016-07-15 17:14:21 -0500

    Styled login page with twitter bootstrap and some custom CSS. Still tweaking failed login message.

--- public/login.php
+++ public/login.php
@@ -9,7 +9,7 @@ function pageController()
 		if (($username == 'guest') && ($password == 'password')) {
 			header('Location: authorized.php');
 		} else {
-			$fail = 'Sorry, username or password not valid.';
+			$fail = 'Sorry, you have entered an invalid username or password.';
 		}
 	}
 
@@ -27,17 +27,48 @@ extract(pageController());
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Login</title>
-</head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="/css/site.css">
 
+</head>
 <body>
-	<form method="POST" action="/login.php">
-		<label for="usernumber">Username: </label>
-		<input id="username" name="username" type="text">
-		<label for="password">Password: </label>
-		<input id="password" name="password" type="password">
-		<button type="submit">Submit</button>
-	</form>
-	<p><?= $fail ?></p>
+	<div class="container">
+			<div id="form-head">
+				<h2>Login: </h2>
+			</div>
+			<div id="form-body">
+				<form method="POST" action="/login.php" role="form">
+					<div class="form-group">
+						<label for="usernumber">Username: </label>
+						<input id="username" name="username" type="text" placeholder="Enter username." autofocus>
+					</div>
+					<div class="form-group">
+						<label for="password">Password: </label>
+						<input id="password" name="password" type="password" placeholder="Enter password.">
+					</div>
+					<button class="btn btn-primary" type="submit" id="button-login"> <span class="glyphicon glyphicon-log-in"></span> Login </button>
+				</form>
+			</div>
+		<div id="alert">
+			<p id="fail"><?= $fail ?></p>
+		</div>
+	</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	<script type="text/javascript">
+		(function() {
+			"use strict";
+
+			var alert = $('#alert');
+			var ptag = $('#fail');
+			if (ptag[0].innerText != '') {
+				alert.addClass('alert, alert-danger').attr('role=\"alert\"');
+			}
+		}());
+	</script>
 </body>
 </html>

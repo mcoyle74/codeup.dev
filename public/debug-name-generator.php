92c4d2185d2bd4efca506d9e90dff71c81ca90ef
Michael Coyle <mcoyle74@gmail.com>
2016-07-15 09:29:07 -0500

    This bug never had a chance. Created empty $data[] array. Moved each shuffle() function from inside array_shift() to its own line because it returns a boolean. Moved array_shift() functions to asssignment of $data[] values. Deleted 'echo' from php in h1 tag.

---
+++ public/debug-name-generator.php
@@ -0,0 +1,39 @@
+<?php
+function pageController()
+{
+	$data = [];
+	$adjectives = ['awesome', 'legendary', 'great', 'cool', 'amazing'];
+	$nouns = ['php', 'javascript', 'css', 'html', 'mysql'];
+	shuffle($adjectives);
+	shuffle($nouns);
+	$data['randomAdjective'] = array_shift($adjectives);
+	$data['randomNoun'] = array_shift($nouns);
+	return $data;
+}
+
+extract(pageController());
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link
+		rel="stylesheet"
+		href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"
+	>
+	<title>Server name generator</title>
+</head>
+<body>
+<div class="container">
+	<h1><?= $randomAdjective ?> <?= $randomNoun ?></h1>
+</div>
+<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js">
+</script>
+</body>
+</html>

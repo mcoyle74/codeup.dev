8ff1b558cc669778a07f71c3ff683f4788967e0e
Michael Coyle <mcoyle74@gmail.com>
2016-07-18 14:05:58 -0500

    Created a file logout.php that clears the session and directs user back to login.php.

---
+++ public/logout.php
@@ -0,0 +1,17 @@
+<?php
+
+function pageController()
+{
+	session_start();
+
+	function clearSession()
+	{
+		session_unset();
+		session_regenerate_id(true);
+	}
+
+	clearSession();
+	header('Location: login.php');
+}
+
+?>

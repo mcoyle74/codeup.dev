9b9efc402bd4d7cf36e3a9a6c26a64eabbbf307c
Michael Coyle <mcoyle74@gmail.com>
2016-07-22 10:48:13 -0500

    Completed had() and get() Input class methods.

--- Input.php
+++ Input.php
@@ -10,7 +10,7 @@ class Input
      */
     public static function has($key)
     {
-        // TODO: Fill in this function
+        return isset($_REQUEST[$key]);
     }
 
     /**
@@ -22,7 +22,7 @@ class Input
      */
     public static function get($key, $default = null)
     {
-        // TODO: Fill in this function
+        return self::has($key) ? $_REQUEST[$key] : $default;
     }
 
     ///////////////////////////////////////////////////////////////////////////

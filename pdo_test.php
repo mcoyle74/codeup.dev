4a6cdb4ed343fbbcc06c5fd48050facc42fa51fb
Michael Coyle <mcoyle74@gmail.com>
2016-08-02 10:54:01 -0500

    Updated files to use constants for db host, name, user and password.

--- pdo_test.php
+++ pdo_test.php
@@ -1,5 +1,10 @@
 <?php
 
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'employees');
+define('DB_USER', 'codeup');
+define('DB_PASS', 'codeup42');
+
 require 'db_connect.php';
 
 echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";

1214d8e5332acccad552626176758917a4ab141c
Michael Coyle <mcoyle74@gmail.com>
2016-08-02 11:51:23 -0500

    Called $dbc->exec() with query to create 'national_parks' table with id, name, location, date_established and area_in_acres.

--- park_migration.php
+++ park_migration.php
@@ -13,4 +13,15 @@ $query = 'DROP TABLE IF EXISTS national_parks';
 
 $dbc->exec($query);
 
+$query = 'CREATE TABLE national_parks(
+		id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+		name VARCHAR (100) NOT NULL,
+		location VARCHAR (100) NOT NULL,
+		date_established DATE NOT NULL,
+		area_in_acres DOUBLE NOT NULL,
+		PRIMARY KEY (id)
+	)';
+
+$dbc->exec($query);
+
 ?>

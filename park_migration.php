d4c9821b482fb40bfc7c8e62a663edd75f1ff30c
Michael Coyle <mcoyle74@gmail.com>
2016-08-02 11:30:54 -0500

    Created file, park_migration.php, to set up connection parameters for park_db and parks_user, requiring db_connect.php

---
+++ park_migration.php
@@ -0,0 +1,12 @@
+<?php
+
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'parks_db');
+define('DB_USER', 'parks_user');
+define('DB_PASS', 'parks42');
+
+require 'db_connect.php';
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+
+?>

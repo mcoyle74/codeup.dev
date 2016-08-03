9c29d247234a4b8876e39e048454f7963159ce34
Michael Coyle <mcoyle74@gmail.com>
2016-08-03 15:09:07 -0500

    Updated park_migration.php to include a new column, 'description,' to the national_parks table.

--- park_migration.php
+++ park_migration.php
@@ -16,6 +16,7 @@ $query = 'CREATE TABLE national_parks(
 		location VARCHAR (100) NOT NULL,
 		date_established DATE NOT NULL,
 		area_in_acres DOUBLE NOT NULL,
+		description VARCHAR (255) NOT NULL,
 		PRIMARY KEY (id)
 	)';
 

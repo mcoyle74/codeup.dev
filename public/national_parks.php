10ee1c95a03350d0a202857a5e2b5cf8e3716a88
Michael Coyle <mcoyle74@gmail.com>
2016-08-03 15:05:20 -0500

    Added bootstrap styling via required head.phtml and script.phtml files. Updated html with appropiate classes/attributes on the pagination.

--- public/national_parks.php
+++ public/national_parks.php
@@ -6,6 +6,8 @@ require_once '../src/Input.php';
 
 function pageController($dbc)
 {
+	$title = 'National Parks';
+
 	$query = 'SELECT * FROM national_parks';
 	$limit = 4;
 	$page = Input::get('page') < 1 ? 1 : Input::get('page', 1);
@@ -15,7 +17,9 @@ function pageController($dbc)
 	$parks = $dbc->query($query)->fetchAll(PDO::FETCH_ASSOC);
 
 	return [
-		'parks' => $parks
+		'title' => $title,
+		'parks' => $parks,
+		'page' => $page
 	];
 }
 
@@ -26,28 +30,48 @@ extract(pageController($dbc));
 <!DOCTYPE html>
 <html>
 <head>
-	<title>National Parks</title>
+	<?php require_once '../partials/head.phtml'; ?>
 </head>
 <body>
-	<h1>National Parks</h1>
-	<table>
-		<tr>
-			<th>Name</th>
-			<th>Location</th>
-			<th>Date Est.</th>
-			<th>Area (acres)</th>
-		</tr>
-		<?php foreach ($parks as $park): ?>
+	<div class ="container">
+		<h1>National Parks</h1>
+		<table>
 			<tr>
-				<td><?= $park['name']; ?></td>
-				<td><?= $park['location']; ?></td>
-				<td><?= $park['date_established']; ?></td>
-				<td><?= $park['area_in_acres']; ?></td>
+				<th>Name</th>
+				<th>Location</th>
+				<th>Date Est.</th>
+				<th>Area (acres)</th>
 			</tr>
-		<?php endforeach; ?>
-	</table>
-	<?php for ($i = 1; $i <= 15; $i++): ?>
-		<a href="national_parks.php?page=<?= $i; ?>"><?= $i; ?></a>
-	<?php endfor; ?>
+			<?php foreach ($parks as $park): ?>
+				<tr>
+					<td><?= $park['name']; ?></td>
+					<td><?= $park['location']; ?></td>
+					<td><?= $park['date_established']; ?></td>
+					<td><?= $park['area_in_acres']; ?></td>
+				</tr>
+			<?php endforeach; ?>
+		</table>
+		<nav aria-label="Page navigation">
+			<ul class="pagination">
+				<?php if ($page > 1): ?>
+					<li>
+						<a href="national_parks.php?page=<?= $page - 1; ?>" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a>
+					</li>
+				<?php endif; ?>
+	
+				<?php for ($i = 1; $i <= 15; $i++): ?>
+					<li><a href="national_parks.php?page=<?= $i; ?>"><?= $i; ?></a></li>
+				<?php endfor; ?>
+	
+				<?php if ($page < 15): ?>
+					<li>
+						<a href="national_parks.php?page=<?= $page + 1; ?>" aria-label="Next"><span aria-hidden="true">&raquo;</span></a>
+					</li>
+				<?php endif; ?>
+			</ul>
+		</nav>
+	</div>
+
+	<?php require_once '../partials/scripts.phtml'; ?>
 </body>
 </html>

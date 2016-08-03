e752ef48d1f2150dc5d906b61f2828e0e328df15
Michael Coyle <mcoyle74@gmail.com>
2016-08-03 15:42:00 -0500

    Updated parks_seeder to use prepare() and bindValue(). Added values for description column to parks.php.

--- park_seeder.php
+++ park_seeder.php
@@ -7,15 +7,16 @@ $query = 'TRUNCATE national_parks';
 
 $dbc->exec($query);
 
-$insert = 'INSERT INTO national_parks (name, location, date_established, area_in_acres) VALUES';
+$stmt = $dbc->prepare('INSERT INTO national_parks (name, location, date_established, area_in_acres, description) VALUES (:name, :location, :date_established, :area_in_acres, :description)');
 
-foreach ($parks as $key => $park) {
-	if ($key != 0) {
-		$insert .= ',';
-	}
-	$insert .= " ('" . $park['name'] . "', '" . $park['location'] . "', '" . $park['date_established'] . "', '" . $park['area_in_acres'] . "')";
+foreach ($parks as $park) {
+	$stmt->bindValue(':name', $park['name'], PDO::PARAM_STR);
+	$stmt->bindValue(':location', $park['location'], PDO::PARAM_STR);
+	$stmt->bindValue(':date_established', $park['date_established'], PDO::PARAM_STR);
+	$stmt->bindValue(':area_in_acres', $park['area_in_acres'], PDO::PARAM_STR);
+	$stmt->bindValue(':description', $park['description'], PDO::PARAM_STR);
+
+	$stmt->execute();
 }
-
-$dbc->exec($insert);
 
 ?>

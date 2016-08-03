838b82221564ed9292a2721b0d9b750104c026c1
Michael Coyle <mcoyle74@gmail.com>
2016-08-03 10:50:15 -0500

    Moved db connection constants to their own file and updated park_migration to require it

--- park_migration.php
+++ park_migration.php
@@ -1,9 +1,6 @@
 <?php
 
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'parks_db');
-define('DB_USER', 'parks_user');
-define('DB_PASS', 'PARKS42');
+require_once 'park_constants.php';
 
 require 'db_connect.php';
 

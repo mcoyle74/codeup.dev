4d50d00542a55253c0e2ed43317aab6d509b7ff3
Michael Coyle <mcoyle74@gmail.com>
2016-08-08 11:45:34 -0500

    Added isPost() method to Input.php that checks super global $_SERVER.

--- src/Input.php
+++ src/Input.php
@@ -25,6 +25,11 @@ class Input
 		return self::has($key) ? $_REQUEST[$key] : $default;
 	}
 
+	public static function isPost()
+	{
+		return $_SERVER['REQUEST_METHOD'] === 'POST';
+	}
+
 	public static function getString($key)
 	{
 		if (gettype(self::get($key)) != 'string') {

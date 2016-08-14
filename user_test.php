833888a5d4061901bbe0f06097a5371ff44e1410
Michael Coyle <mcoyle74@gmail.com>
2016-08-14 14:01:40 -0500

    Found user by id.

--- user_test.php
+++ user_test.php
@@ -2,11 +2,12 @@
 
 require_once 'User.php';
 
-$user = new User();
-$user->name = 'Bob Harris';
-$user->email = 'bobharris@callmebob.org';
-$user->password = password_hash('secret', PASSWORD_DEFAULT);
-$user->save();
-var_dump($user);
+// $user = new User();
+// $user->name = 'Bob Harris';
+// $user->email = 'bobharris@callmebob.org';
+// $user->password = password_hash('secret', PASSWORD_DEFAULT);
+// $user->save();
 
+$user = User::find(1);
+var_dump($user);
 ?>
